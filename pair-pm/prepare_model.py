8244b427a35f0ff629b4fe760488ef8ca06c3038
Yurun Yuan <yurun.yuan@outlook.com>
2024-07-20 17:10:51 -0500

    Config llama.yaml to use abs path

--- pair-pm/prepare_model.py
+++ pair-pm/prepare_model.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
+import sys
+import yaml
 
 name = 'meta-llama/Meta-Llama-3-8B-Instruct'
 tokenizer_name = name
@@ -15,6 +17,17 @@ tokenizer.pad_token = tokenizer.eos_token
 tokenizer.pad_token_id = tokenizer.eos_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 
-model.save_pretrained(os.environ['LLAMA_MODEL_SAVE_PATH'])
-tokenizer.save_pretrained(os.environ['LLAMA_MODEL_SAVE_PATH'])
+model_save_path = os.environ['LLAMA_MODEL_SAVE_PATH']
+model.save_pretrained(model_save_path)
+tokenizer.save_pretrained(model_save_path)
 
+config_template_path = sys.argv[1]
+with open(config_template_path, 'r') as f:
+    config = yaml.safe_load(f)
+
+config['base_model'] = model_save_path
+config['output_dir'] = os.environ['LLAMA_MODEL_OUTPUT_PATH']
+config['dataset_prepared_path'] = os.environ['LLAMA_MODEL_PREPARE_PATH']
+
+with open(config_template_path, 'w') as f:
+    yaml.dump(config, f)
